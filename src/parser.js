c1d3c665dc83a869cbdd45c787a6ed21dc7326fe
Ai-Lin Liou <alincode@gmail.com>
2019-03-05 21:14:16 +0800

    fixed resolve path

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,13 @@
+const replaceContent = require('solc-import').replaceContent;
+const resolver = require('./resolver');
+
 module.exports = async function (importPath) {
   const [, url,] = require('./index').match.exec(importPath);
   try {
     const response = await fetch(url, { method: 'GET' });
-    const data = await response.text();
+    let data = await response.text();
     if (!response.ok || response.status !== 200) throw Error('Content ' + data);
+    data = replaceContent(data, importPath, resolver);
     return data;
   } catch (error) {
     throw error;
--- test/https.js
+++ test/https.js
@@ -13,6 +13,13 @@ describe('https', () => {
     content.length.should.be.above(50);
   });
 
+  it('found 2', async () => {
+    const path = 'https://gist.githubusercontent.com/ryestew/add9633c2b0101f6fda0aadcfe350f60/raw/f4b63d9b95be22c4b676477c42cb49c22db04c3a/ERC20Mintable.sol';
+    let content = await resolveHttp.parser(path);
+    content.should.be.a('string');
+    content.length.should.be.above(50);
+  });
+
   it('no found', async () => {
     const path = 'https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/master/contracts/math/SafeMath2.sol';
     try {
